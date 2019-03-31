40817acc0121c1ae43b28e5c2e9baaec2129e339
admin <admin@goragod.com>
2019-03-31 14:35:17 +0700

    upgrade 2.0.3

--- install/settings/config.php
+++ install/settings/config.php
@@ -3,7 +3,7 @@
 /* config.php */
 
 return array(
-    'version' => '2.0.2',
+    'version' => '2.0.3',
     'web_title' => 'Kotchasan',
     'web_description' => 'Admin Framework',
     'timezone' => 'Asia/Bangkok',
